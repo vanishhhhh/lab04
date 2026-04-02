e2a91f38e2722d2f0a604ec61e21a9c4a7a44db4
vanishhhhh <vanpanvit@gmail.com>
2026-04-02 09:51:32 +0300

    first

---
+++ lab04/_build/hello_world/CMakeFiles/hello_world.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/vanish/vanishhhhh/workspace/projects/lab03/hello_world/hello_world.cpp" "hello_world/CMakeFiles/hello_world.dir/hello_world.cpp.o" "gcc" "hello_world/CMakeFiles/hello_world.dir/hello_world.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/vanish/vanishhhhh/workspace/projects/lab03/_build/formatter_ex_lib/CMakeFiles/formatter_ex.dir/DependInfo.cmake"
+  "/home/vanish/vanishhhhh/workspace/projects/lab03/_build/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
